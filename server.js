5f94fb46176e000f8b37a179068b989bebd6aeb5
lautarxtomas <lautaro.martinez88@mi.unc.edu.ar>
2022-10-04 11:51:16 -0300

    desafio con ejs

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const routerProducts = Router()
 const apiContainer = require ('./src/containers/apiContainer') // import de clase constructora
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.get('/', (req, res) => {
+    res.render('index', { products });
+});
+
 app.use('/api/products', routerProducts)
-app.use(express.static('public'))
 
 
 const products = []
